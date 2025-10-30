28452ca00d58cfe6b25bff10f4a4285ab5f8fb20
천명준 <ecdw3239@naver.com>
2025-10-30 17:11:40 +0900

    Fixsth

--- android/ongil/app/src/main/java/kr/co/ongil/presentation/MainActivity.kt
+++ android/ongil/app/src/main/java/kr/co/ongil/presentation/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import kr.co.ongil.presentation.theme.OngilTheme
+import kr.co.ongil.presentation.ui.Atest.PlayGroundMJ
+import kr.co.ongil.presentation.ui.Atest.PlayGroundSH
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             OngilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // ✅ PlayGroundMJ 실행
+                PlayGroundMJ()
+                PlayGroundSH()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OngilTheme {
-        Greeting("Android")
     }
 }
