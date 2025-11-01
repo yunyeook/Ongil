e01500630897996b8860de0e1cfbe7733952ab8e
gu <skk3010g@gmail.com>
2025-11-01 20:20:01 +0900

    chore: http 통신 관련 설정
    - JSON 파싱 시 한글 인코딩 설정 추가
    - Security 관련 접근 가능 엔드포인트 설정
    
    S13P31A305-86

--- backend/ongil/src/main/java/kr/co/ongil/global/config/JacksonConfig.java
+++ backend/ongil/src/main/java/kr/co/ongil/global/config/JacksonConfig.java
@@ -1,19 +1,38 @@
 package kr.co.ongil.global.config;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
-public class JacksonConfig {
+public class JacksonConfig implements WebMvcConfigurer {
 
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.registerModule(new JavaTimeModule());
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
         return mapper;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converters.add(0, converter);
     }
 }
