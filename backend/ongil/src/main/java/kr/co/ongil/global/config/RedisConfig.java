32325b0b6f112ffeb58a76fdca1b34515a7119d5
이도준 <rabent0207@gmail.com>
2025-11-03 17:29:27 +0900

    Edit RedisConfig.java

--- backend/ongil/src/main/java/kr/co/ongil/global/config/RedisConfig.java
+++ backend/ongil/src/main/java/kr/co/ongil/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package kr.co.ongil.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
