2bf061f0a7f888d4bb329c2b2e69952a829881de
이도준 <rabent0207@gmail.com>
2025-11-05 08:56:51 +0900

    Edit build.gradle.kts

--- android/ongil/build.gradle.kts
+++ android/ongil/build.gradle.kts
@@ -1,10 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+apply(from = "publish-local-aars.gradle.kts")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.sonarqube") version "4.4.1.3373"
     alias(libs.plugins.android.library) apply false
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("${rootDir}/local-maven-repo") }  // 추가
+    }
 }
 
 sonar {
