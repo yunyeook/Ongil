b7cdf221231f7101521aa08bee42751c74f869f8
이도준 <rabent0207@gmail.com>
2025-11-10 15:12:55 +0900

    feat: 대쉬보드 관련 로직들 집계파트 제외하고 완성 - 컨트롤러 서비스 레포지토리 등등 완성 S13P31A305-341

---
+++ backend/ongil/src/main/java/kr/co/ongil/domain/patient/dashboard/repository/DashboardRepository.java
@@ -0,0 +1,14 @@
+package kr.co.ongil.domain.patient.dashboard.repository;
+
+import kr.co.ongil.domain.patient.dashboard.entity.DashboardCalc;
+import kr.co.ongil.domain.patient.safezone.entity.SafeZone;
+import kr.co.ongil.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DashboardRepository extends JpaRepository<DashboardCalc, Integer> {
+    Optional<DashboardCalc> findByPatient(User patient);
+
+    Optional<DashboardCalc> findByPatientId(Integer patientId);
+}
