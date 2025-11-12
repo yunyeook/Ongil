4bd589dfa8cb30f55510f2187539652bd7fcfcae
이도준 <rabent0207@gmail.com>
2025-11-12 10:38:13 +0900

    feat: 대쉬보드 로직 수정, p6spy 추가

--- backend/ongil/src/main/java/kr/co/ongil/domain/user/repository/UserRepository.java
+++ backend/ongil/src/main/java/kr/co/ongil/domain/user/repository/UserRepository.java
@@ -2,10 +2,13 @@ package kr.co.ongil.domain.user.repository;
 
 import java.util.List;
 import kr.co.ongil.domain.user.entity.User;
+import kr.co.ongil.domain.user.entity.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
@@ -14,7 +17,9 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     boolean existsByPhoneNumber(String phoneNumber);
 
-    @org.springframework.data.jpa.repository.Query(
+    Set<Long> findIdByUserType(UserType userType);
+
+    @Query(
         "select u.phoneNumber from User u where u.id = :id"
     )
     Optional<String> findPhoneNumberById(@org.springframework.data.repository.query.Param("id") Integer id);
